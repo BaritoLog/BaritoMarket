e44b0fae603be2b3de5447fdffd4d807a64be8f2
Bherly Novrandy <pushm0v.development@gmail.com>
2018-03-01 14:29:10 +0700

    Fix devbox.sh

--- devbox.sh
+++ devbox.sh
@@ -1,5 +1,6 @@
 gem install bundle
-cp -n config/database.yml.sample config/database.yml
+cp -n config/application.yml.example config/application.yml
+cp -n config/database.yml.example config/database.yml
 bundle install
 bundle exec jbundle install
 RAILS_ENV=development bundle exec rake db:create db:migrate prepare_local
