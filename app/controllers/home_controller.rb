f4f28ea7d857a6b436111a3ca4697f9dd66e49b1
Bherly Novrandy <pushm0v.development@gmail.com>
2018-04-05 16:03:29 +0700

    Add Home view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < BaseController
   def index
-
+    unless @current_user.nil?
+      @clients = Client.where(user_id: @current_user.id)
+    else
+      @clients = Client.all
+    end
   end
 end
