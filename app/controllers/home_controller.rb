9bb8c76743bacfcd069c3618c498fdc7a8d1cca2
Bherly Novrandy <pushm0v.development@gmail.com>
2018-04-12 15:28:53 +0700

    Fix home#index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,5 @@
 class HomeController < BaseController
   def index
-    unless @current_user.nil?
-      @clients = Client.where(user_id: @current_user.id)
-    else
-      @clients = Client.all
-    end
+    redirect_to :controller => 'apps', :action => 'index'
   end
 end
