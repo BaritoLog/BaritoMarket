07029016ec4cc18b0796e696f061b169e001fb5c
Giovanni Sakti <giosakti@gmail.com>
2018-05-17 10:04:28 +0700

    [gio/tara] remove unused classes, will recreate these later if necessary

--- app/models/blueprint_node.rb
+++
@@ -1,9 +0,0 @@
-class BlueprintNode
-  attr_accessor :name, :chef_node_config
-  
-  def initialize args
-    args.each do |k,v|
-      instance_variable_set("@#{k}", v) unless v.nil?
-    end
-  end
-end
