331e25f2ca910f49c621f36fa90329252d4c1832
Giovanni Sakti <giosakti@gmail.com>
2018-05-18 14:17:49 +0700

    [gio/tara] create chef solo client

---
+++ app/services/chef_solo_provisioner.rb
@@ -0,0 +1,50 @@
+require 'open3'
+
+class ChefSoloProvisioner
+  attr_accessor :chef_repo_dir
+
+  def initialize(chef_repo_dir, opts = {})
+    @chef_repo_dir = chef_repo_dir
+    @nodes_dir = opts[:nodes_dir] || File.join(@chef_repo_dir, "nodes")
+  end
+
+  def provision!(host, username, opts = {})
+    private_key = ""
+    private_key = "-i #{opts[:private_key]}" if opts[:private_key]
+    opts[:attrs] ||= {run_list: []}
+
+    tmp_file = "/tmp/#{SecureRandom.uuid}.json"
+    File.open(tmp_file, 'w+') { |f| 
+      f.flock(File::LOCK_EX)
+      f.puts(opts[:attrs].to_json) 
+    }
+
+    # Construct command stack
+    cmd_stack = []
+    cmd_stack << "cd #{@chef_repo_dir} &&"
+    cmd_stack << "knife solo bootstrap"
+    cmd_stack << "#{private_key}"
+    cmd_stack << "#{username}@#{host}"
+    cmd_stack << "#{tmp_file}"
+
+    stdout_str, error_str, status = Open3.capture3(cmd_stack.join(' '))
+    
+    # Put node json file
+    node_file = "#{@nodes_dir}/#{host}.json"
+    FileUtils.cp tmp_file, node_file
+    FileUtils.rm tmp_file
+
+    if status.success?
+      return {
+        'success' => true,
+        'status' => status
+      }
+    else
+      return {
+        'success' => false,
+        'status' => status,
+        'error' => error_str
+      }
+    end
+  end
+end
---
+++ spec/services/chef_solo_provisioner_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe ChefSoloProvisioner do
+  before(:each) do
+  end
+
+  describe '#process!' do
+    it 'should create node json attributes file' do
+      # Don't actually run knife solo
+      status = double('status')
+      allow(status).to receive(:success?).and_return(true)
+      allow(Open3).to receive(:capture3).and_return([Object.new, '', status])
+
+      provisioner = ChefSoloProvisioner.new("#{Rails.root}/tmp/chef")
+      provisioner.provision!('localhost', 'user', attrs: @test_attributes)
+      expect(File).to exist("#{Rails.root}/tmp/chef/nodes/localhost.json")
+    end
+  end
+end
