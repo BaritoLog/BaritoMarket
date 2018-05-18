70340837af34440d18af860c3fbe7cf534698f74
Giovanni Sakti <giosakti@gmail.com>
2018-05-18 14:41:58 +0700

    [gio/tara] fix build error, have to create directory if it's not exist

--- spec/services/chef_solo_provisioner_spec.rb
+++ spec/services/chef_solo_provisioner_spec.rb
@@ -11,6 +11,9 @@ RSpec.describe ChefSoloProvisioner do
       allow(status).to receive(:success?).and_return(true)
       allow(Open3).to receive(:capture3).and_return([Object.new, '', status])
 
+      # Create tmp directory if not exist
+      FileUtils.mkdir_p "#{Rails.root}/tmp/chef/nodes"
+
       provisioner = ChefSoloProvisioner.new("#{Rails.root}/tmp/chef")
       provisioner.provision!('localhost', 'user', attrs: @test_attributes)
       expect(File).to exist("#{Rails.root}/tmp/chef/nodes/localhost.json")
