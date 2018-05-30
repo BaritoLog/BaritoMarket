601897c929f9b9af3d462dd8446baf0819d2d5e3
Siddarth R <siddarth.ramaswamy@go-jek.com>
2018-05-30 16:19:02 +0530

    Controller clean up

--- app/controllers/admin/index_controller.rb
+++
@@ -1,6 +0,0 @@
-class Admin::IndexController < Admin::BaseController
-
-  def index
-
-  end
-end
--- app/controllers/home_controller.rb
+++
@@ -1,5 +0,0 @@
-class HomeController < BaseController
-  def index
-    redirect_to :controller => 'apps', :action => 'index'
-  end
-end
