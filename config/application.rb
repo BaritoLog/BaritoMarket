d760a91fc2fe9b177baffcb3525b0906c36e0970
Siddarth R <siddarth.ramaswamy@go-jek.com>
2018-05-30 16:12:33 +0530

    Upgraded to Rails 5 & removed unused dependencies

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require File.expand_path('../boot', __FILE__)
+require File.expand_path('boot', __dir__)
 
 require 'rails/all'
 
@@ -21,6 +21,5 @@ module BaritoMarket
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
   end
 end
