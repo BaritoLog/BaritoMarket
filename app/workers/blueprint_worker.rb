9db19f15dea3b4a6d0174dfef5440ea2a3be6905
Giovanni Sakti <giosakti@gmail.com>
2018-06-07 13:24:04 +0700

    [gio/bherly] specify lxc host on env vars for now

--- app/workers/blueprint_worker.rb
+++ app/workers/blueprint_worker.rb
@@ -7,7 +7,17 @@ class BlueprintWorker
       content = File.read(filepath)
       begin
         blueprint_hash = JSON.parse(content)
-        BlueprintProcessor.new(blueprint_hash).process!
+        BlueprintProcessor.new(
+          blueprint_hash,
+
+          # TODO: remove these references when sauron can
+          # schedule containers automatically
+          sauron_host: Figaro.env.sauron_host,
+          container_host: Figaro.env.container_host,
+          container_host_name: Figaro.env.container_host_name,
+          private_key_name: Figaro.env.container_private_key,
+          username: Figaro.env.container_username
+        ).process!
       rescue JSON::ParseError, StandardError => ex
         logger.warn "Exception: #{ex}"
       end
