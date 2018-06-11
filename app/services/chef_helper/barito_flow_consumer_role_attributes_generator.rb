8668b6ba77e0c6c56efe01be8e0de7963fb0abd6
Giovanni Sakti <giosakti@gmail.com>
2018-06-11 10:03:06 +0700

    [gio/tara] update configuration to be sent to barito-flow cookbook

--- app/services/chef_helper/barito_flow_consumer_role_attributes_generator.rb
+++ app/services/chef_helper/barito_flow_consumer_role_attributes_generator.rb
@@ -5,13 +5,13 @@ module ChefHelper
                    elasticsearch_host, 
                    consul_hosts, 
                    opts = {})
-      @app_secret_key = app_secret_key
-      @kafka_hosts = kafka_hosts
-      @kafka_port = opts[:kafka_port] || 9092
+      @app_secret_key     = app_secret_key
+      @kafka_hosts        = kafka_hosts
+      @kafka_port         = opts[:kafka_port] || 9092
       @elasticsearch_host = elasticsearch_host
       @elasticsearch_port = opts[:elasticsearch_port] || 9200
-      @consul_hosts = consul_hosts
-      @role_name = opts[:role_name] || 'barito-flow-consumer'
+      @consul_hosts       = consul_hosts
+      @role_name          = opts[:role_name] || 'barito-flow-consumer'
     end
 
     def generate
@@ -19,18 +19,22 @@ module ChefHelper
         map{ |kafka_host| "#{kafka_host}:#{@kafka_port}" }
       kafka_hosts_and_port = kafka_hosts_and_port.join(',')
       elasticsearch_url = "http://#{@elasticsearch_host}:#{@elasticsearch_port}"
+      push_metric_url = "http://#{Figaro.env.market_end_point}/api/increase_log_count"
 
       {
         'barito-flow' => {
           'consumer' => {
             'env_vars' => {
-              'BARITO_KAFKA_BROKERS' => kafka_hosts_and_port,
-              'BARITO_KAFKA_GROUP_ID' => 'barito-group',
-              'BARITO_KAFKA_CONSUMER_TOPICS' => 'barito-log',
-              'BARITO_ELASTICSEARCH_URL' => elasticsearch_url,
-              'BARITO_PUSH_METRIC_URL' => "http://#{Figaro.env.market_end_point}/api/increase_log_count",
-              'BARITO_PUSH_METRIC_TOKEN' => @app_secret_key,
-              'BARITO_PUSH_METRIC_INTERVAL' => '30s',
+              'BARITO_CONSUL_URL'                 => "http://#{@consul_hosts.sample}",
+              'BARITO_CONSUL_KAFKA_NAME'          => 'kafka',
+              'BARITO_CONSUL_ELASTICSEARCH_NAME'  => 'elasticsearch',
+              'BARITO_KAFKA_BROKERS'              => kafka_hosts_and_port,
+              'BARITO_KAFKA_GROUP_ID'             => 'barito-group',
+              'BARITO_KAFKA_CONSUMER_TOPICS'      => 'barito-log',
+              'BARITO_ELASTICSEARCH_URL'          => elasticsearch_url,
+              'BARITO_PUSH_METRIC_URL'            => push_metric_url,
+              'BARITO_PUSH_METRIC_TOKEN'          => @app_secret_key,
+              'BARITO_PUSH_METRIC_INTERVAL'       => '30s',
             }
           }
         },
--- app/services/chef_helper/barito_flow_producer_role_attributes_generator.rb
+++ app/services/chef_helper/barito_flow_producer_role_attributes_generator.rb
@@ -1,11 +1,11 @@
 module ChefHelper
   class BaritoFlowProducerRoleAttributesGenerator
     def initialize(kafka_hosts, consul_hosts, opts = {})
-      @kafka_hosts = kafka_hosts
-      @kafka_port = opts[:kafka_port] || 9092
+      @kafka_hosts  = kafka_hosts
+      @kafka_port   = opts[:kafka_port] || 9092
       @consul_hosts = consul_hosts
-      @tps_limit = opts[:tps_limit] || 10
-      @role_name = opts[:role_name] || 'barito-flow-producer'
+      @tps_limit    = opts[:tps_limit] || 10
+      @role_name    = opts[:role_name] || 'barito-flow-producer'
     end
 
     def generate
@@ -17,10 +17,12 @@ module ChefHelper
         'barito-flow' => {
           'producer' => {
             'env_vars' => {
-              'BARITO_PRODUCER_ADDRESS' => ':8080',
-              'BARITO_KAFKA_BROKERS' => kafka_hosts_and_port,
+              'BARITO_PRODUCER_ADDRESS'     => ':8080',
+              'BARITO_CONSUL_URL'           => "http://#{@consul_hosts.sample}",
+              'BARITO_CONSUL_KAFKA_NAME'    => 'kafka',
+              'BARITO_KAFKA_BROKERS'        => kafka_hosts_and_port,
               'BARITO_KAFKA_PRODUCER_TOPIC' => 'barito-log',
-              'BARITO_PRODUCER_MAX_TPS' => @tps_limit
+              'BARITO_PRODUCER_MAX_TPS'     => @tps_limit
             }
           }
         },
