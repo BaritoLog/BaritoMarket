ac5dba91d674eb569c123de6f27eedf62b77b430
opan <opan.neutron@gmail.com>
2018-07-09 14:43:11 +0700

    [opan/bherly] Remove unused action in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,4 @@ class UsersController < ApplicationController
     @users = User.where("username ILIKE :q OR email ILIKE :q", { q: "%#{params[:q]}%" })
     render json: @users
   end
-
-  def index
-  end
-
-  def show
-    @user = current_user
-  end
 end
