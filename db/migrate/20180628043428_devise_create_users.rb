103e9fd0ff13d6ec1e48fc3d92ae5d4bbfa4504d
opan <opan.neutron@gmail.com>
2018-07-09 14:55:49 +0700

    [opan/bherly] Update user migration file

--- db/migrate/20180628043428_devise_create_users.rb
+++ db/migrate/20180628043428_devise_create_users.rb
@@ -5,7 +5,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       ## CAS authenticatable
       t.string :username, null: false, unique: true
-      t.string :email, default: ""
+      t.string :auth_token, default: ''
+      t.boolean :admin, default: false, null: false
+
+      ## Database authenticatable
+      t.string :email, default: ''
+      t.string :encrypted_password, default: ''
 
       ## Recoverable
       # t.string   :reset_password_token
@@ -37,7 +42,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
     end
 
     add_index :users, :username, unique: true
-    # add_index :users, :email,                unique: true
+    add_index :users, :email,                unique: true
     # add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
