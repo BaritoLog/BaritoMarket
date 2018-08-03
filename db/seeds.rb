c98bad46e07b16f675ec043ef18b3a71f0b510b7
opan <opan.neutron@gmail.com>
2018-08-03 18:12:40 +0700

    [opan] Update seed to create user properly

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,6 @@
-Group.create(name: 'barito-superadmin')
+group = Group.create(name: 'barito-superadmin')
+user = User.create(email: 'superadmin@barito.com', username: 'superadmin', password: '123456', password_confirmation: '123456')
+GroupUser.create(user: user, group: group)
 
 ['admin', 'owner', 'member'].each do |role|
   AppGroupRole.create(name: role)
