7be544d4d2ca4f4c14e76fbc2fa6667e259b290f
opan <opan.neutron@gmail.com>
2018-08-15 11:24:40 +0700

    [opan] Add request spec for increase_log_count api

--- app/controllers/api/apps_controller.rb
+++ app/controllers/api/apps_controller.rb
@@ -36,7 +36,7 @@ class Api::AppsController < Api::BaseController
   end
 
   def increase_log_count
-    dog = Dogapi::Client.new(Figaro.env.datadog_api_key)
+    @app = BaritoApp.find_by_secret_key(params[:token])
     @app.increase_log_count(params[:new_log_count])
     dog.emit_point("barito.#{@app.name}", params[:new_log_count])
     render json: {
--- spec/requests/api/apps_spec.rb
+++ spec/requests/api/apps_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe 'Apps API', type: :request do
-  describe 'Profile API' do
-    let(:headers) do
-      { 'ACCEPT' => 'application/json', 'HTTP_ACCEPT' => 'application/json' }
-    end
+  let(:headers) do
+    { 'ACCEPT' => 'application/json', 'HTTP_ACCEPT' => 'application/json' }
+  end
 
+  describe 'Profile API' do
     it 'should return profile information of registered app' do
       app_group = create(:app_group)
       infrastructure = create(:infrastructure, app_group: app_group)
@@ -21,38 +21,51 @@ RSpec.describe 'Apps API', type: :request do
       expect(json_response['updated_at']).to eq(app_updated_at)
     end
 
-    it 'should return 401 for invalid token' do
-      secret_key = SecureRandom.uuid.gsub(/\-/, '')
-      error_msg = "Unauthorized: #{secret_key} is not a valid App Token"
-      get api_profile_path, params: { token: secret_key }, headers: headers
-      json_response = JSON.parse(response.body)
-      expect(json_response['code']).to eq(401)
-      expect(json_response['errors']).to eq([error_msg])
+    context 'when invalid token' do
+      it 'should return 401' do
+        secret_key = SecureRandom.uuid.gsub(/\-/, '')
+        error_msg = "Unauthorized: #{secret_key} is not a valid App Token"
+        get api_profile_path, params: { token: secret_key }, headers: headers
+        json_response = JSON.parse(response.body)
+        expect(json_response['code']).to eq(401)
+        expect(json_response['errors']).to eq([error_msg])
+      end
     end
 
-    it 'should return 422, when token is not provided' do
-      error_msg = 'Invalid Params: token is a required parameter'
-      get api_profile_path, params: { token: '' }, headers: headers
-      json_response = JSON.parse(response.body)
-      expect(json_response['code']).to eq(422)
-      expect(json_response['errors']).to eq([error_msg])
+    context 'when token is not provided' do
+      it 'should return 422' do
+        error_msg = 'Invalid Params: token is a required parameter'
+        get api_profile_path, params: { token: '' }, headers: headers
+        json_response = JSON.parse(response.body)
+        expect(json_response['code']).to eq(422)
+        expect(json_response['errors']).to eq([error_msg])
+      end
     end
   end
 
-  describe 'Dogapi API' do
-    let(:api_key) {'API_KEY'}
-    let(:dog) { Dogapi::Client.new(api_key) }
-    let(:api_url) {'api.datadoghq.com/api/v1'}
+  describe 'Increase Log count API' do
+    context 'when valid token' do
+      it 'should return 200' do
+        app_group = create(:app_group)
+        app = create(:barito_app, app_group: app_group)
 
-    describe '#emit_point' do
-      it 'post metric to the datadog api' do
-        METRIC = 'test.metric'.freeze
-        POINT = 10
+        post api_increase_log_count_path, params: { token: app.secret_key, new_log_count: 10 }, headers: headers
+        json_response = JSON.parse(response.body)
 
-        url = api_url + '/series'
-        stub_request(:post, /#{url}/).to_return(body: '{}').then.to_raise(StandardError)
-        expect(dog.send(:emit_point, METRIC, POINT)).to eq ['200', {}]
+        expect(response.status).to eq 200
+        expect(json_response['log_count']).to eq(10)
+      end
+    end
 
+    context 'when invalid token' do
+      it 'should return 401' do
+        secret_key = SecureRandom.uuid.gsub(/\-/, '')
+        error_msg = "Unauthorized: #{secret_key} is not a valid App Token"
+        post api_increase_log_count_path, params: { token: secret_key, new_log_count: 10 }, headers: headers
+        json_response = JSON.parse(response.body)
+
+        expect(json_response['code']).to eq 401
+        expect(json_response['errors']).to eq([error_msg])
       end
     end
   end
