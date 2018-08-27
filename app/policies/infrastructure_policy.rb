ef12ba76ba2c11ed2ba00bb0e799c816821fe6b9
opan <opan.neutron@gmail.com>
2018-08-27 14:03:22 +0700

    [opan] Fix infrastructure policy

--- app/policies/infrastructure_policy.rb
+++ app/policies/infrastructure_policy.rb
@@ -13,7 +13,7 @@ class InfrastructurePolicy < ApplicationPolicy
   end
 
   def exists?
-    return true if get_user_groups and !@record.nil?
+    return true if get_user_groups
 
     app_group_ids = AppGroupUser.where(user: user).pluck(:app_group_id)
     app_group_ids.include?(record.id)
--- spec/requests/api/infrastructures_spec.rb
+++ spec/requests/api/infrastructures_spec.rb
@@ -9,17 +9,17 @@ RSpec.describe 'App API', type: :request do
     it 'should return profile information of registered app when supplied cluster name' do
       app_group = create(:app_group)
       infrastructure = create(:infrastructure, app_group: app_group)
-      # app_updated_at = app.updated_at.strftime(Figaro.env.timestamp_format)
+
       get api_profile_by_cluster_name_path,
         params: { cluster_name: infrastructure.cluster_name },
         headers: headers
       json_response = JSON.parse(response.body)
+
       %w[name app_group_name capacity cluster_name consul_host status provisioning_status].each do |key|
         expect(json_response.key?(key)).to eq(true)
         expect(json_response[key]).to eq(infrastructure.send(key.to_sym))
       end
       expect(json_response.key?('updated_at')).to eq(true)
-      # expect(json_response['updated_at']).to eq(app_updated_at)
     end
   end
 
@@ -37,6 +37,7 @@ RSpec.describe 'App API', type: :request do
         expect(response.status).to eq 200
       end
     end
+
     context 'when invalid username or invalid cluster_name' do
       it 'should return 401' do
         app_group = create(:app_group)
