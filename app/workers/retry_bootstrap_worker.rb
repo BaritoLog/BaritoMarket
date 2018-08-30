032637df1bb7390653e93ddb77b933863839fe29
Muhammad Fadli Nurhasan <fadli.nurhasan@go-jek.com>
2018-08-30 08:56:03 +0700

    [fadli/opan] update infrastructure status if got error while bootstraping

--- app/workers/retry_bootstrap_worker.rb
+++ app/workers/retry_bootstrap_worker.rb
@@ -2,9 +2,9 @@ class RetryBootstrapWorker
   include Sidekiq::Worker
 
   def perform(infrastructure_component_id)
-    begin
-      infrastructure_component = InfrastructureComponent.find(
+    infrastructure_component = InfrastructureComponent.find(
         infrastructure_component_id)
+    begin
       bootstrapper = BaritoBlueprint::Bootstrapper.new(
         infrastructure_component.infrastructure,
         ChefSoloBootstrapper.new(Figaro.env.chef_repo_dir),
@@ -12,9 +12,10 @@ class RetryBootstrapWorker
         private_key_name: Figaro.env.container_private_key,
         username: Figaro.env.container_username,
       )
-      bootstrapper.bootstrap_instance!(infrastructure_component)
+      bootstrapper.bootstrap_instance!(infrastructure_component, true)
     rescue JSON::ParserError, StandardError => ex
       logger.warn "Exception: #{ex}"
+      infrastructure_component.update_status('BOOTSTRAP_ERROR', ex.to_s)
     end
   end
 end
