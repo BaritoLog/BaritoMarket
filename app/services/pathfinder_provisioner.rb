ba2aab3f9bfd2a4992b58348e88e759e39613b33
Giovanni Sakti <giosakti@gmail.com>
2018-08-30 08:27:20 +0700

    [gio/tara] create new client for pathfinder

---
+++ app/services/pathfinder_provisioner.rb
@@ -0,0 +1,68 @@
+class PathfinderProvisioner
+  attr_accessor :pathfinder_host, :pathfinder_cluster
+
+  def initialize(pathfinder_host, pathfinder_cluster, opts = {})
+    @pathfinder_host = pathfinder_host
+    @pathfinder_cluster = pathfinder_cluster
+    @image = opts[:image] || '16.04'
+  end
+
+  def provision!(hostname)
+    req = Typhoeus::Request.new(
+      "#{@pathfinder_host}/api/v1/ext_app/containers.json",
+      method: :post,
+      params: {
+        'cluster_name' => @pathfinder_cluster,
+      },
+      body: {
+        'container' => {
+          'hostname' => hostname,
+          'image' => @image
+        }
+      }.to_json,
+      headers: {
+        'Content-Type' => 'application/json',
+        'X-Auth-Token' => ''
+      }
+    )
+    req.run
+    
+    if req.response.success?
+      return respond_success(req.response)
+    else
+      return respond_error(req.response)
+    end
+  end
+
+  def show_container(hostname)
+    req = Typhoeus::Request.new(
+      "#{@pathfinder_host}/api/v1/ext_app/containers/#{hostname}",
+      method: :get,
+      params: {
+        'cluster_name' => @pathfinder_cluster,
+      },
+      headers: {
+        'Content-Type' => 'application/json',
+        'X-Auth-Token' => ''
+      }
+    )
+    req.run
+    return respond_success(req.response)
+  end
+
+  private
+    def respond_success(response)
+      body = JSON.parse(response.body)
+      { 
+        'success' => true,
+        'data' => {
+          'ipaddress' => body.dig('data', 'ipaddress')
+        }
+      }
+    end
+
+    def respond_error(response)
+      body = JSON.parse(response.body)
+      { 'success' => false, 'error' => body['errors'] }
+    end
+end
---
+++ spec/services/pathfinder_provisioner_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe PathfinderProvisioner do
+  describe '#provision!' do
+    before(:all) do
+      @pathfinder_host = '127.0.0.1:3000'
+      @pathfinder_cluster = 'barito'
+
+      # Mock Pathfinder API
+      stub_request(:post, "http://#{@pathfinder_host}/api/v1/ext_app/containers.json").
+        with(
+          query: {
+            'cluster_name' => @pathfinder_cluster
+          },
+          body: {
+            'container' => {
+              'hostname' => 'test-01',
+              'image' => '16.04',
+            }
+          }.to_json,
+          headers: {
+            'Content-Type' => 'application/json',
+            'X-Auth-Token' => '',
+          }
+        ).to_return({
+          status: 201,
+          headers: {
+            'Content-Type' => 'application/json',
+          },
+          body: {
+            'error' => '',
+            'data' => {
+              'ipaddress' => '127.0.0.1'
+            }
+          }.to_json
+        })
+    end
+
+    it 'should make necessary calls to Pathfinder and return the response' do
+      pathfinder_provisioner = PathfinderProvisioner.new(@pathfinder_host, @pathfinder_cluster)
+      provision_result = pathfinder_provisioner.provision!('test-01')
+      expect(provision_result).to eq({
+        'success' => true,
+        'data' => {'ipaddress' => '127.0.0.1'},
+      })
+    end
+  end
+end
