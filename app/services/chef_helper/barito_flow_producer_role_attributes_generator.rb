f214c5607c627160561a03fa7cc378282c8e2840
Giovanni Sakti <giosakti@gmail.com>
2018-09-10 19:32:06 +0700

    bump version of barito-flow

--- app/services/chef_helper/barito_flow_producer_role_attributes_generator.rb
+++ app/services/chef_helper/barito_flow_producer_role_attributes_generator.rb
@@ -18,7 +18,7 @@ module ChefHelper
       {
         'barito-flow' => {
           'producer' => {
-            'version' => 'v0.8.2',
+            'version' => 'v0.8.3',
             'env_vars' => {
               'BARITO_PRODUCER_ADDRESS'     => ':8080',
               'BARITO_CONSUL_URL'           => "http://#{@consul_hosts.sample}:#{Figaro.env.default_consul_port}",
