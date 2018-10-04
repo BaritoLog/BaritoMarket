fa807a4f22e93c49e1d39369de204b358bac3f55
ekarisky <ekarisky@gmail.com>
2018-10-04 11:48:23 +0700

    remove app_group from barito metric datadog

--- app/services/datadog_listener.rb
+++ app/services/datadog_listener.rb
@@ -16,9 +16,9 @@ class DatadogListener
 
     @dog.batch_metrics do
       @dog.emit_point(
-        "barito.app_group.log_count", app_group.log_count, :app_group => "#{app_group.name}", :app_name => "#{app.name}")
+        "barito.log_count", app_group.log_count, :app_group => "#{app_group.name}", :app_name => "#{app.name}")
       @dog.emit_point(
-        "barito.app_group.log_throughput", app_log_throughput, :app_group => "#{app_group.name}", :app_name => "#{app.name}")
+        "barito.log_throughput", app_log_throughput, :app_group => "#{app_group.name}", :app_name => "#{app.name}")
     end
   end
 
