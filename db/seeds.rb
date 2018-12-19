734cab0b1105c4bfadcb9190fdb7b70362179947
Tara Baskara <tara.baskara@gmail.com>
2018-12-19 16:19:37 +0700

    [tara/fadli] added global-viewer seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,13 @@
-group = Group.create(name: 'barito-superadmin')
+superadmin_group = Group.create(name: 'barito-superadmin')
+global_viewer_group = Group.create(name: 'global-viewer')
 
 if Figaro.env.enable_cas_integration == 'true'
   user = User.create(email: 'superadmin@barito.com', username: 'superadmin')
 else
   user = User.create(email: 'superadmin@barito.com', username: 'superadmin', password: '123456', password_confirmation: '123456')
 end
-GroupUser.create(user: user, group: group)
+GroupUser.create(user: user, group: superadmin_group)
+GroupUser.create(user: user, group: global_viewer_group)
 
 ['admin', 'owner', 'member'].each do |role|
   AppGroupRole.create(name: role)
