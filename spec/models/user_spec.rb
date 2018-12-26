0f8dcf29915e341dcacaaac33e94b97a3f12de7f
Tara Baskara <tara.baskara@gmail.com>
2018-12-26 16:08:46 +0700

    [tara] added model spec to check global viewer group

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,4 +14,14 @@ RSpec.describe User, type: :model do
       expect(User.find_by_username_or_email('test@test.com')).to eq @user
     end
   end
+
+  describe '#add_global_viewer_group' do
+    let(:config) { YAML.load_file("#{Rails.root}/config/application.yml") }
+    let(:group) { create(:group, name: 'global-viewer') }
+    let(:user) { create(:user, username: 'test_user', email: 'test@test.com') }
+      
+    it "shoud add global viewer group to user if global viewer field in env is true" do 
+      expect(user.groups.include?(group).to_s).to eq(config["GLOBAL_VIEWER"])
+    end
+  end
 end
