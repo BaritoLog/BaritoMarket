c14df4c7bac6a6a7aee9dc9273b87b2d38af8760
Tara Baskara <tara.baskara@gmail.com>
2018-12-27 13:22:22 +0700

    [tara/fadli] updated spec for global viewer

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,11 +17,10 @@ RSpec.describe User, type: :model do
 
   describe '#add_global_viewer_group' do
     let(:config) { YAML.load_file("#{Rails.root}/config/application.yml") }
-    let(:group) { create(:group, name: 'global-viewer') }
     let(:user) { create(:user, username: 'test_user', email: 'test@test.com') }
-      
+
     it "shoud add global viewer group to user if global viewer field in env is true" do 
-      expect(user.groups.include?(group).to_s).to eq(config["GLOBAL_VIEWER"])
+      expect(user.groups.any? { |g| g[:name] == "global-viewer" }.to_s).to eq(config["GLOBAL_VIEWER"])
     end
   end
 end
