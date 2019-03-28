f72721be4b64a5a141a5283ea4340c1be083d6ec
Fadli Nurhasan <fadli.nurhasan@gmail.com>
2019-03-28 11:46:25 +0700

    [Fadli|Puja] Create Migrate seed for distributing cluster template to existing infrastructure.

---
+++ db/migrate_seeds/20190326161123_update_infrastructure_cluster_template.rb
@@ -0,0 +1,33 @@
+class UpdateInfrastructureCLusterTemplate < ActiveRecord::Migration[5.2]
+  def up
+    all_infrastructures = Infrastructure.all
+
+    all_infrastructures.each do |infra|
+      next unless infra.cluster_template.nil?
+      infra_name = infra.name.downcase
+      if infra_name.include?("staging") || infra_name.include?("integration")
+        update_attrs(infra, "Staging", infra.capacity)
+      elsif infra_name.include?("production")
+        update_attrs(infra, "Production", infra.capacity)
+      else
+        update_attrs(infra, "Production", infra.capacity)
+      end
+    end
+
+    public
+      def update_attrs(infra, env, capacity)
+        cluster_template_name = env + " - " + capacity.capitalize
+        ct = ClusterTemplate.find_by(name: cluster_template_name)
+
+        infra.update(cluster_template: ct, capacity: ct.name)
+      end
+  end
+
+  def down
+    all_infrastructures = Infrastructure.all
+    all_infrastructures.each do |infra|
+      capacity = infra.capacity.split.last
+      infra.update(cluster_template: nil, capacity: capacity.downcase)
+    end
+  end
+end
