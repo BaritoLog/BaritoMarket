b45176404885d65de45e50788d97e30e19bf7d34
Puja Pramudya <puja@radyalabs.com>
2019-07-02 16:06:52 +0700

    Add space between parameters

--- spec/lib/pathfinder_provisioner_spec.rb
+++ spec/lib/pathfinder_provisioner_spec.rb
@@ -119,7 +119,7 @@ RSpec.describe PathfinderProvisioner do
 
       it 'should make necessary calls to Pathfinder and return the response' do
         pathfinder_provisioner = PathfinderProvisioner.new(@pathfinder_host, @pathfinder_token, @pathfinder_cluster)
-        provision_result = pathfinder_provisioner.reprovision!('test-01', @source,@bootstrappers)
+        provision_result = pathfinder_provisioner.reprovision!('test-01', @source, @bootstrappers)
         expect(provision_result).to eq({
           'success' => true,
           'data' => {'ipaddress' => '127.0.0.1'},
