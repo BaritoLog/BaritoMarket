be813b3f4ba57058c5c7d9fab4f873682a5845a0
Giovanni Sakti <giosakti@gmail.com>
2019-07-09 06:32:54 +0000

    replace deprecated chromedriver-helper gem with webdrivers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,7 @@ require 'webmock/rspec'
 require 'database_cleaner'
 require 'fakeredis/rspec'
 require 'coveralls'
+require 'webdrivers'
 require 'selenium/webdriver'
 require 'simplecov'
 require 'simplecov-console'
@@ -33,7 +34,6 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 Capybara.server = :puma, { Silent: true }
-
 Capybara.register_driver :custom_headless_chrome do |app|
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
     chromeOptions: { args: %w(headless disable-gpu no-sandbox) }
@@ -43,7 +43,6 @@ Capybara.register_driver :custom_headless_chrome do |app|
     browser: :chrome,
     desired_capabilities: capabilities
 end
-
 Capybara.javascript_driver = :custom_headless_chrome
 
 RSpec.configure do |config|
