75d6d9cd47a078408e37f91beb9b7757d8b08f76
Giovanni Sakti <giosakti@gmail.com>
2019-07-09 06:41:48 +0000

    allow download of chromedriver

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -59,7 +59,7 @@ RSpec.configure do |config|
   config.before(:suite) do
     ENV['ENABLE_CAS_INTEGRATION'] = 'true'
 
-    WebMock.disable_net_connect!(allow: ['localhost', '127.0.0.1', /selenium/])
+    WebMock.disable_net_connect!(allow: ['localhost', '127.0.0.1', /selenium/, 'chromedriver.storage.googleapis.com'])
     Sidekiq::Testing.fake!
     DatabaseCleaner.clean_with(:truncation)
 
