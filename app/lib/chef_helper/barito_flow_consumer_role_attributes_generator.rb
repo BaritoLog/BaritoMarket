a7f3ff889ac238c1eab919cd33d4802849d7195b
fadlinurhasan <fadlinurhasan@users.noreply.github.com>
2019-11-01 13:59:42 +0700

    Adjust generate bootstrap attribute to use dns consul
    * Setup kibana config to use es dns instead of specific es url for LB purpose on kibana attribute
    
    Co-authored-by: ekarisky <ekarisky@gmail.com>
    
    * Setup es config to get list of es node, set minimum master node and set replica number.
    
    Co-authored-by: ekarisky <ekarisky@gmail.com>
    
    * Adjust generate attribute on consumer to enable consul dns for kafka and elasticsearch
    
    Co-authored-by: ekarisky <ekarisky@gmail.com>
    
    * Adjust generate attribute on producer to enable consul dns for kafka brokers
    
    Co-authored-by: ekarisky <ekarisky@gmail.com>

--- app/lib/chef_helper/barito_flow_consumer_role_attributes_generator.rb
+++ app/lib/chef_helper/barito_flow_consumer_role_attributes_generator.rb
@@ -1,13 +1,6 @@
 module ChefHelper
   class BaritoFlowConsumerRoleAttributesGenerator < GenericRoleAttributesGenerator
     def initialize(component, infrastructure_components, opts = {})
-      kafka_hosts = fetch_hosts_address_by(
-        infrastructure_components, 'component_type', 'kafka')
-      kafka_port = opts[:kafka_port] || 9092
-      elasticsearch_hosts = fetch_hosts_address_by(
-        infrastructure_components, 'component_type', 'elasticsearch')
-      elasticsearch_port = opts[:elasticsearch_port] || 9200
-
       @consul_hosts = fetch_hosts_address_by(
         infrastructure_components, 'component_type', 'consul')
       @role_name = opts[:role_name] || 'barito-flow-consumer'
@@ -15,8 +8,6 @@ module ChefHelper
       consumer_template = ComponentTemplate.find_by(name: 'barito-flow-consumer')
       @consumer_attrs = get_bootstrap_attributes(consumer_template.bootstrappers)
       @push_metric_url = "#{Figaro.env.market_end_point}/api/increase_log_count"
-      @kafka_hosts = bind_hosts_and_port(kafka_hosts, kafka_port)
-      @elasticsearch_hosts = bind_hosts_and_port(elasticsearch_hosts, elasticsearch_port, "http")
     end
 
     def generate
@@ -26,8 +17,6 @@ module ChefHelper
 
     def update_attrs
       @consumer_attrs['barito-flow']['consumer']['env_vars']['BARITO_CONSUL_URL'] = "http://#{@consul_hosts.sample}:#{Figaro.env.default_consul_port}"
-      @consumer_attrs['barito-flow']['consumer']['env_vars']['BARITO_KAFKA_BROKERS'] = @kafka_hosts
-      @consumer_attrs['barito-flow']['consumer']['env_vars']['BARITO_ELASTICSEARCH_URLS'] = @elasticsearch_hosts
       @consumer_attrs['barito-flow']['consumer']['env_vars']['BARITO_PUSH_METRIC_URL'] = @push_metric_url
 
       @consumer_attrs['consul']['hosts'] = @consul_hosts
