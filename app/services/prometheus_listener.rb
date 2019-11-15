40c2485afb20d10f434c05360b2c8953ad3e3b0d
Daniel <nieltansah@gmail.com>
2019-11-15 12:04:03 +0700

    Fix RoboCop offences in Prometheus listener

--- app/services/prometheus_listener.rb
+++ app/services/prometheus_listener.rb
@@ -26,16 +26,22 @@ class PrometheusListener
     app_name = app.name
 
     @log_count_metric.set(app.log_count, labels: { app_group: app_group.name, app_name: app_name })
-    @log_throughput_metric.set(app_log_throughput, labels: { app_group: app_group.name, app_name: app_name })
+    @log_throughput_metric.set(
+      app_log_throughput, labels: { app_group: app_group.name, app_name: app_name }
+    )
   end
 
   def app_count_changed
-    app_count = AppGroup.joins(:barito_apps, :infrastructure).where.not(infrastructures: { provisioning_status: "DELETED" }).count
+    app_count = AppGroup.joins(:barito_apps, :infrastructure).where.not(
+      infrastructures: { provisioning_status: 'DELETED' },
+    ).count
     @app_count_metric.set(app_count)
   end
 
   def team_count_changed
-    team_count = AppGroup.joins(:infrastructure).where.not(infrastructures: { provisioning_status: "DELETED"}).count
+    team_count = AppGroup.joins(:infrastructure).where.not(
+      infrastructures: { provisioning_status: 'DELETED' },
+    ).count
     @team_count_metric.set(team_count)
   end
 end
