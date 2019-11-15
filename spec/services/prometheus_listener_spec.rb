e004fa9d757b11b727e93d050f7a4556a72438fd
Daniel <nieltansah@gmail.com>
2019-11-15 11:56:20 +0700

    Group tests for per-app metrics

--- spec/services/prometheus_listener_spec.rb
+++ spec/services/prometheus_listener_spec.rb
@@ -7,19 +7,38 @@ RSpec.describe PrometheusListener do
 
   let!(:listener) { described_class.new(registry) }
 
-  describe 'log_count metrics' do
-    subject { registry.get(:barito_market_log_count) }
+  let(:app_group) { create(:app_group) }
+  let!(:app) { create(:barito_app, app_group: app_group, log_count: 10) }
+  let!(:infrastructure1) { create(:infrastructure, app_group: app_group) }
+  let!(:infrastructure2) { create(:infrastructure, app_group: app_group, provisioning_status: "DELETED") }
 
-    it 'should be in registry' do
-      expect(subject).to be_a(Prometheus::Client::Gauge)
+  describe 'per-app metrics' do
+    let(:labels) { { app_group: app.app_group.name, app_name: app.name } }
+
+    describe 'log_count metrics' do
+      subject { registry.get(:barito_market_log_count) }
+
+      it 'should be in registry' do
+        expect(subject).to be_a(Prometheus::Client::Gauge)
+      end
+
+      it 'should be updated whenever log count is changed' do
+        listener.log_count_changed(app.id, 0)
+        expect(subject.get(labels: labels)).to eq(app.log_count)
+      end
     end
-  end
 
-  describe 'log_throughput metrics' do
-    subject { registry.get(:barito_market_log_throughput) }
+    describe 'log_throughput metrics' do
+      subject { registry.get(:barito_market_log_throughput) }
 
-    it 'should be in registry' do
-      expect(subject).to be_a(Prometheus::Client::Gauge)
+      it 'should be in registry' do
+        expect(subject).to be_a(Prometheus::Client::Gauge)
+      end
+
+      it 'should be updated whenever log throughput is changed' do
+        listener.log_count_changed(app.id, 2000.0)
+        expect(subject.get(labels: labels)).to eq(2000.0)
+      end
     end
   end
 
@@ -37,27 +56,6 @@ RSpec.describe PrometheusListener do
     it 'should be in registry' do
       expect(subject).to be_a(Prometheus::Client::Gauge)
     end
-  end
-
-  let(:app_group) { create(:app_group) }
-  let!(:app) { create(:barito_app, app_group: app_group, log_count: 10) }
-  let!(:infrastructure1) { create(:infrastructure, app_group: app_group) }
-  let!(:infrastructure2) { create(:infrastructure, app_group: app_group, provisioning_status: "DELETED") }
-
-  it 'should change whenever log count is changed' do
-    listener.log_count_changed(app.id, 0)
-
-    expect(registry.get(:barito_market_log_count).get(labels: {
-      app_group: app.app_group.name, app_name: app.name
-    })).to eq(app.log_count)
-  end
-
-  it 'should change whenever log throughput is changed' do
-    listener.log_count_changed(app.id, 2000.0)
-
-    expect(registry.get(:barito_market_log_throughput).get(labels: {
-      app_group: app.app_group.name, app_name: app.name
-    })).to eq(2000.0)
   end
 
   it 'should report current app count' do
