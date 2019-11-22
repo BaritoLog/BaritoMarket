33626574a9ccae16ce0465c0b0a8db26c743e7ea
Daniel <nieltansah@gmail.com>
2019-11-22 13:40:39 +0700

    Restrict from adding two same group to an app group

--- app/models/app_group_team.rb
+++ app/models/app_group_team.rb
@@ -2,4 +2,6 @@ class AppGroupTeam < ApplicationRecord
   belongs_to :app_group
   belongs_to :role, class_name: 'AppGroupRole'
   belongs_to :group
+
+  validates :app_group_id, uniqueness: { scope: :group_id }
 end
--- spec/models/app_group_team_spec.rb
+++ spec/models/app_group_team_spec.rb
@@ -1,21 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe AppGroupTeam, type: :model do
+  let(:app_group) { create(:app_group) }
+
   it 'has relation with app group' do
-    app_group = create(:app_group)
     app_group_team = create(:app_group_team, app_group: app_group)
     expect(app_group_team.app_group).to eq(app_group)
   end
 
+  let(:role) { create(:app_group_role) }
+
   it 'has relation with app group role' do
-    app_group_role = create(:app_group_role)
-    app_group_team = create(:app_group_team, role: app_group_role)
-    expect(app_group_team.role).to eq(app_group_role)
+    app_group_team = create(:app_group_team, role: role)
+    expect(app_group_team.role).to eq(role)
   end
 
+  let(:group) { create(:group) }
+
   it 'has relation with group' do
-    group = create(:group)
     app_group_team = create(:app_group_team, group: group)
     expect(app_group_team.group).to eq(group)
+  end
+
+  context 'validation' do
+    it 'rejects adding two same group in an app group' do
+      create(:app_group_team, app_group: app_group, group: group)
+      expect(build(:app_group_team, app_group: app_group, group: group)).not_to be_valid
+    end
   end
 end
