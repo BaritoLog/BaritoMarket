dca99083bb547f6c00a4a1345f902826bdf1eab6
Daniel <nieltansah@gmail.com>
2019-11-26 14:18:47 +0700

    Apply rubocop fixes on spec/models/user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,8 +19,10 @@ RSpec.describe User, type: :model do
     let(:config) { YAML.load_file("#{Rails.root}/config/application.yml") }
     let(:user) { create(:user, username: 'test_user', email: 'test@test.com') }
 
-    it "shoud add global viewer group to user if global viewer field in env is true" do 
-      expect(user.groups.any? { |g| g[:name] == Figaro.env.global_viewer_role }.to_s).to eq(config["GLOBAL_VIEWER"])
+    it 'shoud add global viewer group to user if global viewer field in env is true' do
+      expect(user.groups.any? do |g|
+        g[:name] == Figaro.env.global_viewer_role
+      end.to_s).to eq(config['GLOBAL_VIEWER'])
     end
   end
 end
