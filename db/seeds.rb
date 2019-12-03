4c00abc1cdc3d1e7b817b978b4f03e2e61771cf3
Puja Pramudya <puja.pramudya@gmail.com>
2019-12-03 11:43:38 +0700

    Modify seed script to add role into group user creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,19 @@
 superadmin_group = Group.create(name: 'barito-superadmin')
 global_viewer_group = Group.create(name: Figaro.env.global_viewer_role)
 
+['admin', 'owner', 'member'].each do |role|
+  AppGroupRole.create(name: role)
+end
+
 if Figaro.env.enable_cas_integration == 'true'
   user = User.create(email: 'superadmin@barito.com', username: 'superadmin')
 else
   user = User.create(email: 'superadmin@barito.com', username: 'superadmin', password: '123456', password_confirmation: '123456')
 end
-GroupUser.create(user: user, group: superadmin_group)
-GroupUser.create(user: user, group: global_viewer_group)
+GroupUser.create(user: user, group: superadmin_group, role: AppGroupRole.find_by_name('admin'))
+GroupUser.create(user: user, group: global_viewer_group, role: AppGroupRole.find_by_name('admin'))
 
-['admin', 'owner', 'member'].each do |role|
-  AppGroupRole.create(name: role)
-end
+
 
 load "#{::Rails.root}/db/seeds/master_data/cluster_template.rb"
 load "#{::Rails.root}/db/seeds/master_data/component_template.rb"
