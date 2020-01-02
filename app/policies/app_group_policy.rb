07b911493095c685a17f82f33626d74a082c651f
Daniel <nieltansah@gmail.com>
2020-01-02 13:18:05 +0700

    Admin can't update app group

--- app/policies/app_group_policy.rb
+++ app/policies/app_group_policy.rb
@@ -12,7 +12,7 @@ class AppGroupPolicy < ApplicationPolicy
   end
 
   def update?
-    barito_superadmin? || user.can_access_app_group?(record, roles: %i(admin))
+    barito_superadmin?
   end
 
   def update_app_group_name?
