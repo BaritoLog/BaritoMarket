f8503ba05fe89f73929c8a7cfe16a7547275254f
Daniel Pintara <nieltansah@gmail.com>
2020-01-10 14:38:05 +0700

    Add test for copyright tag

---
+++ spec/features/misc/copyright_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.feature 'Miscellaneous', type: :feature do
+  describe 'Copyright tag' do
+    scenario 'At copyright year' do
+      Timecop.freeze(2018, 1, 1) do
+        visit root_path
+        expect(page).to have_content('2018 Barito')
+      end
+    end
+  end
+end
