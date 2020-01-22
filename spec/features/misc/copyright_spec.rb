375e1dbc0d88660ed5bb05f8965b794da95c186a
Daniel Pintara <nieltg@users.noreply.github.com>
2020-01-22 14:24:23 +0700

    Auto-copyright for BaritoMarket (#186)
    * Add test for copyright tag
    
    * Automatically change copyright year based on server date
    
    * Don't show copyright range if server's date is less than Barito's 1st copyright year

---
+++ spec/features/misc/copyright_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.feature 'Miscellaneous', type: :feature do
+  describe 'Copyright tag' do
+    scenario 'At copyright year' do
+      Timecop.freeze(2018, 1, 1) do
+        visit root_path
+        expect(page).to have_content('2018 Barito')
+      end
+    end
+
+    scenario 'After copyright year' do
+      Timecop.freeze(2019, 1, 1) do
+        visit root_path
+        expect(page).to have_content('2018-2019 Barito')
+      end
+    end
+
+    scenario 'Before copyright year' do
+      Timecop.freeze(2017, 1, 1) do
+        visit root_path
+        expect(page).to have_content('2018 Barito')
+      end
+    end
+  end
+end
