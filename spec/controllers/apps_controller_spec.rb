8e694c658bb7631d74e1042bf73d4f4eea47c326
Daniel Pintara <nieltansah@gmail.com>
2020-01-28 15:08:29 +0700

    Add test not to update log_retention_days if not superadmin
    Co-authored-by: Beni Budiharto <gnu.iben@gmail.com>

--- spec/controllers/apps_controller_spec.rb
+++ spec/controllers/apps_controller_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe AppsController, type: :controller do
   let(:infrastructure) { create(:infrastructure) }
-  let(:app) { create(:barito_app, app_group: infrastructure.app_group) }
+  let(:app_group) { infrastructure.app_group }
+  let(:app) { create(:barito_app, app_group: app_group) }
 
   let(:user) { create(:user) }
 
@@ -19,6 +20,24 @@ RSpec.describe AppsController, type: :controller do
 
         app.reload
         expect(app.log_retention_days).to eq(10)
+      end
+    end
+
+    context 'as owner' do
+      before :each do
+        set_check_user_groups({ 'groups' => [] })
+
+        role_owner = create(:app_group_role, :owner)
+        create(:app_group_user, user: user, app_group: app_group, role: role_owner)
+        sign_in user
+      end
+
+      it 'does not update log_retention_days' do
+        app.update(log_retention_days: 14)
+        post :update_log_retention_days, params: { id: app.id, barito_app: { log_retention_days: 10 } }
+
+        app.reload
+        expect(app.log_retention_days).to eq(14)
       end
     end
   end
