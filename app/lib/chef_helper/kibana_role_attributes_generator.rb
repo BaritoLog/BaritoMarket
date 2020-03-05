1253d90a32c744052833a8017d328430613920ef
Galuh Estya <galuhestya@gmail.com>
2020-03-05 16:23:08 +0700

    kibana attribute generator should use manifest
    Co-authored-by: Fadli <fadli.nurhasan@gmail.com>

--- app/lib/chef_helper/kibana_role_attributes_generator.rb
+++ app/lib/chef_helper/kibana_role_attributes_generator.rb
@@ -1,13 +1,10 @@
 module ChefHelper
   class KibanaRoleAttributesGenerator < GenericRoleAttributesGenerator
-    def initialize(component, infrastructure_components, opts = {})
-      @consul_hosts = fetch_hosts_address_by(
-        infrastructure_components, 'component_type', 'consul')
+    def initialize(manifest, infrastructure_manifests, opts = {})
+      @consul_hosts = fetch_hosts_adress_manifests_by(infrastructure_manifests,'consul')
       @role_name = opts[:role_name] || 'kibana'
-      @base_path = component.infrastructure.cluster_name
-      @ipaddress = component.ipaddress
-      kibana_template = ComponentTemplate.find_by(name: 'kibana')
-      @kibana_attrs = get_bootstrap_attributes(kibana_template.bootstrappers)
+      @base_path = manifest[:cluster_name]
+      @kibana_attrs = get_bootstrap_attributes(manifest[:definition][:bootstrappers])
     end
 
     def generate
@@ -16,9 +13,9 @@ module ChefHelper
     end
 
     def update_attrs
-      @kibana_attrs['kibana']['config']['server.basePath'] = "/#{@base_path}"
-      @kibana_attrs['consul']['hosts'] = @consul_hosts
-      @kibana_attrs['run_list'] = ["role[#{@role_name}]"]
+      @kibana_attrs[:kibana][:config][:"server.basePath"] = "/#{@base_path}"
+      @kibana_attrs[:consul][:hosts] = @consul_hosts
+      @kibana_attrs[:run_list] = ["role[#{@role_name}]"]
 
       @kibana_attrs
     end
