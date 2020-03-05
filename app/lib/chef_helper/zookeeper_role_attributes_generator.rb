f93572e312b0429f90e77d792055770935b09d58
Galuh Estya <galuhestya@gmail.com>
2020-03-05 15:42:57 +0700

    zookeeper generator should use manifest
    Co-authored-by: Fadli <fadli.nurhasan@gmail.com>

--- app/lib/chef_helper/zookeeper_role_attributes_generator.rb
+++ app/lib/chef_helper/zookeeper_role_attributes_generator.rb
@@ -1,9 +1,9 @@
 module ChefHelper
   class ZookeeperRoleAttributesGenerator < GenericRoleAttributesGenerator
-    def initialize(component, infrastructure_components, opts = {})
-      hosts = fetch_hosts_address_by(
-        infrastructure_components, 'component_type', 'zookeeper')
-      @my_id = (hosts.index(component.ipaddress) || hosts.index(component.hostname)) + 1
+    def initialize(manifest, infrastructure_manifests, opts = {})
+      hosts = fetch_hosts_adress_manifest_by(manifest,'zookeeper')
+      @my_id = (hosts.index("#{manifest[:name]}-01.node.zookeeper")) + 1
+
       @domains = []
       hosts.each_with_index do |host, idx|
         if idx+1 == @my_id
@@ -12,14 +12,12 @@ module ChefHelper
           @domains << "#{idx+1}.zookeeper.service.consul"
         end
       end
-      @consul_hosts = fetch_hosts_address_by(
-        infrastructure_components, 'component_type', 'consul')
-      @role_name = opts[:role_name] || 'zookeeper'
-      @cluster_name = component.infrastructure.cluster_name
-      @hostname = component.hostname
-      @ipaddress = component.ipaddress
-      zookeeper_template = ComponentTemplate.find_by(name: 'zookeeper')
-      @zookeeper_attrs = get_bootstrap_attributes(zookeeper_template.bootstrappers)
+      
+      @consul_hosts = fetch_hosts_adress_manifests_by(infrastructure_manifests,'consul')
+      @role_name = opts[:role_name] || :zookeeper
+      @cluster_name = manifest[:cluster_name]
+      @hostname = manifest[:name]
+      @zookeeper_attrs = get_bootstrap_attributes(manifest[:definition][:bootstrappers])
     end
 
     def generate
@@ -28,17 +26,17 @@ module ChefHelper
     end
 
     def update_attrs
-      @zookeeper_attrs['zookeeper']['hosts'] = @domains
-      @zookeeper_attrs['zookeeper']['my_id'] = @my_id
-      @zookeeper_attrs['consul']['hosts'] = @consul_hosts
-      @zookeeper_attrs['run_list'] = ["role[#{@role_name}]"]
+      @zookeeper_attrs[:zookeeper][:hosts] = @domains
+      @zookeeper_attrs[:zookeeper][:my_id] = @my_id
+      @zookeeper_attrs[:consul][:hosts] = @consul_hosts
+      @zookeeper_attrs[:run_list] = ["role[#{@role_name}]"]
 
-      if Figaro.env.datadog_integration == 'true'
-        @zookeeper_attrs['datadog']['datadog_api_key'] = Figaro.env.datadog_api_key
-        @zookeeper_attrs['datadog']['datadog_hostname'] = @hostname
-        @zookeeper_attrs['datadog']['zk']['instances'][0]['cluster_name'] = @cluster_name
-        @zookeeper_attrs['run_list'] << 'recipe[datadog::default]'
-        @zookeeper_attrs['run_list'] << 'recipe[datadog::zk_datadog]'
+      if Figaro.env.datadog_integration == :true
+        @zookeeper_attrs[:datadog][:datadog_api_key] = Figaro.env.datadog_api_key
+        @zookeeper_attrs[:datadog][:datadog_hostname] = @hostname
+        @zookeeper_attrs[:datadog][:zk][:instances][0][:cluster_name] = @cluster_name
+        @zookeeper_attrs[:run_list] << 'recipe[datadog::default]'
+        @zookeeper_attrs[:run_list] << 'recipe[datadog::zk_datadog]'
       end
 
       @zookeeper_attrs
--- spec/lib/chef_helper/zookeeper_role_attributes_generator_spec.rb
+++ spec/lib/chef_helper/zookeeper_role_attributes_generator_spec.rb
@@ -3,54 +3,121 @@ require 'rails_helper'
 module ChefHelper
   RSpec.describe ZookeeperRoleAttributesGenerator do
     before(:each) do
-      @infrastructure = create(:infrastructure, cluster_name: 'test')
-      @consul_component = create(:infrastructure_component, 
-        infrastructure: @infrastructure,
-        hostname:       'test-consul-01',
-        component_type: 'consul',
-        ipaddress:      '127.0.0.1'
-      )
-      @zookeeper_component = create(:infrastructure_component, 
-        infrastructure: @infrastructure,
-        hostname:       'test-zookeeper-01',
-        component_type: 'zookeeper',
-        ipaddress:      '127.0.0.15'
-      )
+      @zookeeper_manifest = {
+                    "name": "haza-zookeeper",
+                    "cluster_name": "barito",
+                    "type": "zookeeper",
+                    "count": 1,
+                    "definition": {
+                      "container_type": "stateless",
+                      "strategy": "RollingUpdate",
+                      "allow_failure": "false",
+                      "resource": {
+                        "cpu_limit": "0-2",
+                        "mem_limit": "5GB"
+                      },
+                      "source": {
+                        "mode": "pull",
+                        "alias": "lxd-ubuntu-minimal-zookeeper-3.4.12-3",
+                        "remote": {
+                          "name": "barito-registry"
+                        },
+                        "fingerprint": "",
+                        "source_type": "image"                      
+                      },
+                      "bootstrappers": [{
+                        "bootstrap_type": "chef-solo",
+                        "bootstrap_attributes": {
+                          "consul": {
+                            "hosts": [
+
+                            ],
+                            "run_as_server": false
+                          },
+                          "run_list": [
+
+                          ],
+                          "zookeeper": {
+                            "hosts": [
+                              ""
+                            ],
+                            "my_id": ""
+                          }
+                        },
+                        "bootstrap_cookbooks_url": "https://github.com/BaritoLog/chef-repo/archive/master.tar.gz"
+                      }],
+                      "healthcheck": {
+                        "type": "tcp",
+                        "port": 9500,
+                        "endpoint": "",
+                        "payload": "",
+                        "timeout": ""
+                      }
+                    }
+                  }
+      @consul_manifest = {
+                    "name": "haza-consul",
+                    "cluster_name": "barito",
+                    "type": "consul",
+                    "count": 1,
+                    "definition": {
+                      "container_type": "stateless",
+                      "strategy": "RollingUpdate",
+                      "allow_failure": "false",
+                      "source": {
+                        "mode": "pull",
+                        "alias": "lxd-ubuntu-minimal-consul-1.1.0-8",
+                        "remote": {
+                          "name": "barito-registry"
+                        },
+                        "fingerprint": "",
+                        "source_type": "image"                      
+                      },
+                      "resource": {
+                        "cpu_limit": "0-2",
+                        "mem_limit": "500MB"
+                      },
+                      "bootstrappers": [{
+                        "bootstrap_type": "chef-solo",
+                        "bootstrap_attributes": {
+                          "consul": {
+                            "hosts": []
+                          },
+                          "run_list": []
+                        },
+                        "bootstrap_cookbooks_url": "https://github.com/BaritoLog/chef-repo/archive/master.tar.gz"
+                      }],
+                      "healthcheck": {
+                        "type": "tcp",
+                        "port": 9500,
+                        "endpoint": "",
+                        "payload": "",
+                        "timeout": ""
+                      }
+                    }
+                  }
+      @manifests = [@zookeeper_manifest, @consul_manifest]
       @my_id = 1
     end
 
     describe '#generate' do
       it 'should generate zookeeper attributes' do
         zookeeper_attributes = ZookeeperRoleAttributesGenerator.new(
-          @zookeeper_component,
-          @infrastructure.infrastructure_components
+          @zookeeper_manifest,
+          @manifests
         )
         
         attrs = zookeeper_attributes.generate
 
         expect(attrs).to eq({
-            "consul"=>{
-              "hosts"=>["#{@consul_component.ipaddress}"], 
-              "run_as_server"=>false
-            }, 
-            "datadog"=>{
-              "zk"=>{
-                "instances"=>[
-                  {
-                    "host"=>"localhost", 
-                    "port"=>2181, 
-                    "tags"=>[], 
-                    "cluster_name"=>""
-                  }
-                ]
-              },
-              "datadog_api_key"=>"",
-              "datadog_hostname"=>""
+            :consul=>{
+              :hosts=>["#{@consul_manifest[:name]}-01.node.consul"], 
+              :run_as_server=>false
             },
-            "run_list"=>["role[zookeeper]"], 
-            "zookeeper"=>{
-              "hosts"=>["0.0.0.0"], 
-              "my_id"=>@my_id
+            :run_list=>["role[zookeeper]"], 
+            :zookeeper=>{
+              :hosts=>["0.0.0.0"], 
+              :my_id=>@my_id
             }
           }
         )
