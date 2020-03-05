f76e721864a85d95d9f204e7abda5a1b78769d18
Galuh Estya <galuhestya@gmail.com>
2020-03-05 15:43:29 +0700

    blueprint processor should use manifest
    Co-authored-by: Fadli <fadli.nurhasan@gmail.com>

--- app/workers/blueprint_worker.rb
+++ app/workers/blueprint_worker.rb
@@ -2,17 +2,15 @@ class BlueprintWorker
   include Sidekiq::Worker
   sidekiq_options retry: false
 
-  def perform(instances, opts = {})
+  def perform(opts = {})
     begin
       BaritoBlueprint::Processor.new(
-        instances,
         infrastructure_id: opts["infrastructure_id"],
         pathfinder_host: Figaro.env.pathfinder_host,
         pathfinder_token: Figaro.env.pathfinder_token,
         pathfinder_cluster: Figaro.env.pathfinder_cluster,
-        chef_repo_dir: Figaro.env.chef_repo_dir,
-        username: Figaro.env.container_username
-      ).process_check!
+        chef_repo_dir: Figaro.env.chef_repo_dir
+      ).process!
     rescue JSON::ParserError, StandardError => ex
       logger.warn "Exception: #{ex}"
     end
--- spec/services/barito_blueprint/processor_spec.rb
+++ spec/services/barito_blueprint/processor_spec.rb
@@ -4,20 +4,7 @@ module BaritoBlueprint
   RSpec.describe Processor do
     before(:each) do
       @infrastructure = create(:infrastructure)
-      @instances = [
-          {
-            'name' => 'd-trac-consul-01',
-            'type' => 'consul',
-            'seq' => 1,
-          },
-          {
-            'name' => 'd-trac-yggdrasil-01',
-            'type' => 'yggdrasil',
-            'seq' => 0,
-          },
-        ]
       @processor = Processor.new(
-        @instances,
         infrastructure_id: @infrastructure.id,
         chef_repo_dir: Figaro.env.chef_repo_dir,
       )
@@ -27,18 +14,17 @@ module BaritoBlueprint
     describe '#process!' do
       before(:each) do
         provisioner = double
-        allow(provisioner).to(receive(:provision_instances!).and_return(true))
-        allow(provisioner).to(receive(:check_and_update_instances).
+        allow(provisioner).to(receive(:bulk_apply!).
           and_return(true))
         allow(Provisioner).to receive(:new).and_return(provisioner)
 
         bootstrapper = double
-        allow(bootstrapper).to(receive(:bootstrap_instances!).and_return(true))
+        allow(bootstrapper).to(receive(:generate_manifests!).and_return(true))
         allow(Bootstrapper).to receive(:new).and_return(bootstrapper)
       end
 
       it 'should save consul_host when the process is finished' do
-        @processor.process_check!
+        @processor.process!
         @infrastructure.reload
         expect(@infrastructure.consul_host).not_to eq nil
       end
