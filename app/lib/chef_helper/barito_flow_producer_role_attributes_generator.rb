aacb8ac5edd0f21b4206f4ec258c0f352b09c75b
Galuh Estya <galuhestya@gmail.com>
2020-03-06 10:40:43 +0700

    barito flow producer should use manifest
    Co-authored-by: Fadli <fadli.nurhasan@gmail.com>

--- app/lib/chef_helper/barito_flow_producer_role_attributes_generator.rb
+++ app/lib/chef_helper/barito_flow_producer_role_attributes_generator.rb
@@ -1,13 +1,12 @@
 module ChefHelper
   class BaritoFlowProducerRoleAttributesGenerator < GenericRoleAttributesGenerator
-    def initialize(component, infrastructure_components, opts = {})
-      @consul_hosts = fetch_hosts_address_by(
-        infrastructure_components, 'component_type', 'consul')
-      @max_tps = component.infrastructure.options['max_tps'] || 10
+    def initialize(manifest, infrastructure_manifests, opts = {})
+      @consul_hosts = fetch_hosts_address_manifests_by(
+        infrastructure_manifests, 'consul')
+      barito_flow_producer_manifest = manifest[:definition][:bootstrappers][0][:bootstrap_attributes][:'barito-flow'][:producer]
+      @max_tps = barito_flow_producer_manifest[:env_vars][:BARITO_PRODUCER_MAX_TPS] || 10
       @role_name = opts[:role_name] || 'barito-flow-producer'
-      @ipaddress = component.ipaddress
-      producer_template = ComponentTemplate.find_by(name: 'barito-flow-producer')
-      @producer_attrs = get_bootstrap_attributes(producer_template.bootstrappers)
+      @producer_attrs = get_bootstrap_attributes(manifest[:definition][:bootstrappers])
     end
 
     def generate
@@ -16,10 +15,10 @@ module ChefHelper
     end
 
     def update_attrs
-      @producer_attrs['barito-flow']['producer']['env_vars']['BARITO_PRODUCER_MAX_TPS'] = @max_tps
+      @producer_attrs[:"barito-flow"][:producer][:env_vars][:BARITO_PRODUCER_MAX_TPS] = @max_tps
 
-      @producer_attrs['consul']['hosts'] = @consul_hosts
-      @producer_attrs['run_list'] = ["role[#{@role_name}]"]
+      @producer_attrs[:consul][:hosts] = @consul_hosts
+      @producer_attrs[:run_list] = ["role[#{@role_name}]"]
 
       @producer_attrs
     end
