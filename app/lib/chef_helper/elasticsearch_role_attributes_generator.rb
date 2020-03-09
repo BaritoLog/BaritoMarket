d311dbcef95e47dbe67aed15ff6b48e5cb4d75a2
Galuh Estya <galuhestya@gmail.com>
2020-03-09 17:17:41 +0700

    manifest count should be integer
    Co-authored-by: Fadli <fadli.nurhasan@gmail.com>

--- app/lib/chef_helper/elasticsearch_role_attributes_generator.rb
+++ app/lib/chef_helper/elasticsearch_role_attributes_generator.rb
@@ -6,14 +6,14 @@ module ChefHelper
 
       @role_name = opts[:role_name] || 'elasticsearch'
       @cluster_name = manifest[:cluster_name]
-      @hostname = manifest[:name]
+      @hostname = generate_pf_meta("container_hostname")
       @port = opts[:port] || 9200      
-      if manifest[:count] <= 1
+      if manifest[:count].to_i <= 1
         @index_number_of_replicas = 0
         @minimum_master_nodes = 1
       else
         @index_number_of_replicas = 1
-        @minimum_master_nodes = (manifest[:count]/2 + 1).floor
+        @minimum_master_nodes = (manifest[:count].to_i/2 + 1).floor
       end
 
       elastic_template = ComponentTemplate.find_by(name: 'elasticsearch')
