40b764aaf6a6a0db97f80be276dc77d912da888e
Galuh Estya <galuhestya@gmail.com>
2020-03-13 16:36:39 +0700

    manifest should be auto bulk_apply-ed after edit
    Co-authored-by: Fadli <fadli.nurhasan@gmail.com>

---
+++ app/workers/update_manifests_worker.rb
@@ -0,0 +1,19 @@
+class UpdateManifestsWorker
+  include Sidekiq::Worker
+
+  def perform(infrastructure)
+    begin
+      provisioner = BaritoBlueprint::Provisioner.new(
+        infrastructure,
+        PathfinderProvisioner.new(
+          Figaro.env.pathfinder_host,
+          Figaro.env.pathfinder_token,
+          Figaro.env.pathfinder_cluster,
+      )
+      provisioner.bulk_apply!
+    rescue JSON::ParserError, StandardError, NoMethodError => ex
+      logger.warn "Exception: #{ex}"
+      infrastructure.update_provisioning_status('DEPLOYMENT_ERROR')
+    end
+  end
+end
