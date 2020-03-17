d94516b2288d1d0abd9601703e0c6d8d6e58603d
Daniel Pintara <nieltansah@gmail.com>
2020-03-17 15:22:12 +0700

    Fix ComponentTemplate factory

--- spec/factories/component_templates.rb
+++ spec/factories/component_templates.rb
@@ -1,17 +1,26 @@
 FactoryBot.define do
   factory :component_template do
-    name                  Faker::Lorem.word
-    bootstrappers       {
-                          [{
-                            "bootstrap_type": "chef-solo",
-                            "bootstrap_cookbooks_url": "",
-                            "bootstrap_attributes": {
-                              "consul": {
-                                "hosts": []
-                              },
-                              "run_list": []
-                            }
-                          }]
-                        }
+    name    Faker::Lorem.word
+    source  { {
+                "source_type": "image",  # can be image, migration or copy
+                "mode": "pull",          # can be local or pull. default is pull.
+                "remote": {
+                  "name": "barito-registry"
+                },
+                "fingerprint": "",
+                "alias": "lxd-consul-1.1.0-3"
+            } }
+    bootstrappers {
+                    [{
+                      "bootstrap_type": "chef-solo",
+                      "bootstrap_cookbooks_url": "",
+                      "bootstrap_attributes": {
+                        "consul": {
+                          "hosts": []
+                        },
+                        "run_list": []
+                      }
+                    }]
+                  }
   end
 end
