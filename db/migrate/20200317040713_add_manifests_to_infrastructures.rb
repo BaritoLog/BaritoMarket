b9c5b43879997bdd092ba4308eb0d74735053843
Daniel Pintara <nieltansah@gmail.com>
2020-03-18 18:06:15 +0700

    Update add_manifests_to_infrastructures migration with tests

--- db/migrate/20200317040713_add_manifests_to_infrastructures.rb
+++ db/migrate/20200317040713_add_manifests_to_infrastructures.rb
@@ -4,22 +4,47 @@ class AddManifestsToInfrastructures < ActiveRecord::Migration[5.2]
 
     reversible do |direction|
       direction.up do
+        Infrastructure.connection.schema_cache.clear!
+        Infrastructure.reset_column_information
+
         Infrastructure.all.each do |infrastructure|
           manifests = Hash.new
 
           infrastructure.infrastructure_components.map do |component|
             type = component.component_type
-            is_stateful = type.include? ["zookeeper", "kafka", "elasticsearch"]
+            is_stateful = ["zookeeper", "kafka", "elasticsearch"].include? type
 
-            if manifests.has_key?(type)
-              manifests[type]["count"] += 1
+            if manifests.key?(type)
+              manifests[type][:desired_num_replicas] += 1
+              manifests[type][:min_available_replicas] += 1
             else
+              cluster_name = infrastructure.cluster_name
               manifests[type] = {
-                count: 1,
+                name: "#{cluster_name}-#{type}",
+                cluster_name: Figaro.env.pathfinder_cluster,
+                desired_num_replicas: 1,
+                min_available_replicas: 0,
                 type: type,
                 definition: {
                   source: component.source,
-                  bootstrappers: component.bootstrappers,
+                  bootstrappers: component.bootstrappers.map do |bootstrapper|
+                    if bootstrapper["bootstrap_type"] == "chef-solo"
+                      bootstrapper["bootstrap_attributes"]["consul"]["hosts"] = build_pf_meta(
+                        "deployment_ip_addresses", deployment_name: "#{cluster_name}-consul"
+                      )
+                    end
+
+                    if type == "zookeeper"
+                      bootstrapper["bootstrap_attributes"]["zookeeper"]["my_id"] = build_pf_meta(
+                        "container_id"
+                      )
+                      bootstrapper["bootstrap_attributes"]["zookeeper"]["hosts"] = build_pf_meta(
+                        "deployment_host_sequences", host: "zookeeper.service.consul"
+                      )
+                    end
+
+                    bootstrapper
+                  end,
                   container_type: is_stateful ? "stateful" : "stateless",
                   allow_failure: !is_stateful
                 }
@@ -33,5 +58,13 @@ class AddManifestsToInfrastructures < ActiveRecord::Migration[5.2]
         end
       end
     end
+  end
+
+  private
+  def build_pf_meta(method_name, **kwargs)
+    uri = URI(method_name)
+    uri.query = URI.encode_www_form(kwargs) unless kwargs.empty?
+
+    "$pf-meta:#{uri.to_s}"
   end
 end
