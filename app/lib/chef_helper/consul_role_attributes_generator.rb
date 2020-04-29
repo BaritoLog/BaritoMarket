9592c8426cb01d8aaabe4d44edecbba4750ca7c1
Fadli Nurhasan <fadli.nurhasan@gmail.com>
2020-04-29 11:36:44 +0700

    add consul hosts_count for bootstrap_expect attr
    Co-authored-by: galuhest <galuhestya@gmail.com>

--- app/lib/chef_helper/consul_role_attributes_generator.rb
+++ app/lib/chef_helper/consul_role_attributes_generator.rb
@@ -1,6 +1,7 @@
 module ChefHelper
   class ConsulRoleAttributesGenerator < GenericRoleAttributesGenerator
     def initialize(manifest, infrastructure_manifests, opts = {})
+      @manifest = manifest
       @hosts = generate_pf_meta("deployment_ip_addresses", {deployment_name: "#{manifest['name']}"})
       @role_name = opts[:role_name] || 'consul'
       consul_template = DeploymentTemplate.find_by(name: 'consul')
@@ -14,6 +15,7 @@ module ChefHelper
 
     def update_attrs
       @consul_attrs["consul"]["hosts"] = @hosts
+      @consul_attrs["consul"]["hosts_count"] = @manifest['desired_num_replicas']
       @consul_attrs["run_list"] = ["role[#{@role_name}]"]
       @consul_attrs
     end
