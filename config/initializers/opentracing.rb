da9793bdb90411ac69e8bb26b50ab7f434ac1cc4
Ranjeet Singh <ranjeet@raeoks.com>
2020-12-09 13:59:58 +0530

    [BaritoLog/BaritoMarket#214] Reformat config/opentracing.rb

--- config/initializers/opentracing.rb
+++ config/initializers/opentracing.rb
@@ -1,7 +1,9 @@
-OpenTracing.global_tracer = Jaeger::Client.build(service_name: Figaro.env.JAEGER_SERVICE_NAME,
+# noinspection RubyResolve
+OpenTracing.global_tracer = Jaeger::Client.build(
+  service_name: Figaro.env.JAEGER_SERVICE_NAME,
   reporter: Jaeger::Reporters::RemoteReporter.new(
     sender: Jaeger::HttpSender.new(
-      url:  Figaro.env.JAEGER_ENDPOINT,
+      url: Figaro.env.JAEGER_ENDPOINT,
       encoder: Jaeger::Encoders::ThriftEncoder.new(service_name: Figaro.env.JAEGER_SERVICE_NAME)
     ),
     flush_interval: 10
@@ -17,5 +19,5 @@ OpenTracing.global_tracer = Jaeger::Client.build(service_name: Figaro.env.JAEGER
     sampler: Jaeger::Samplers::Probabilistic.new(rate: 0.01),
     host: Figaro.env.JAEGER_SAMPLING_HOST,
     port: Figaro.env.JAEGER_SAMPLING_PORT
-    )
+  )
 )
