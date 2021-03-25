decd27877e4ae139761e6ca4727f4f3e1ae74b06
bugbounce <35751394+bugbounce@users.noreply.github.com>
2021-03-25 15:49:46 +0530

    Add order to helm cluster templates

--- app/models/helm_cluster_template.rb
+++ app/models/helm_cluster_template.rb
@@ -1,4 +1,5 @@
 class HelmClusterTemplate < ApplicationRecord
+  default_scope { order(name: :asc) }
   validates_uniqueness_of :name
   validates_presence_of :max_tps, :name
   validates :values, helm_values: true
