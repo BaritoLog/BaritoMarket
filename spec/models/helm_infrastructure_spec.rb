713f0bbd753cc4ba8d4165113d82de5a8b65f0d9
Fadli Nurhasan <fadli.nurhasan@gmail.com>
2021-06-23 12:57:57 +0700

    mend
    Co-authored-by: tara.baskara <tara.baskara@gmail.com>

--- spec/models/helm_infrastructure_spec.rb
+++ spec/models/helm_infrastructure_spec.rb
@@ -2,10 +2,21 @@ require 'rails_helper'
 
 RSpec.describe HelmInfrastructure, type: :model do
   subject { create(:helm_infrastructure) }
+  let(:app_group) { create(:app_group) }
+  let(:helm_cluster_template) { create(:helm_cluster_template) }
 
   it 'raises error if invalid override values' do
     subject.override_values = ""
     expect(subject.valid?).to eq false
+  end
+
+  it 'should create the helm_infrastructure' do
+    helm_infrastructure = HelmInfrastructure.setup(
+      app_group_id: app_group.id,
+      helm_cluster_template_id: helm_cluster_template.id,
+    )
+
+    expect(helm_infrastructure.persisted?).to eq(true)
   end
 
 end
