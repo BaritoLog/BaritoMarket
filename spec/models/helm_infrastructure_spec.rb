5ad5941d7fbd990a95653121fc311fa9cf3f8af6
Ranjeet Singh <ranjeet@raeoks.com>
2021-07-23 06:13:48 +0530

    refactor(ruby): modify helm infrastructure spec to reflect duplicate cluster names issue.
    Reviewed-By: Project Barito Contributors

--- spec/models/helm_infrastructure_spec.rb
+++ spec/models/helm_infrastructure_spec.rb
@@ -31,21 +31,34 @@ RSpec.describe HelmInfrastructure, type: :model do
       helm_infrastructure = HelmInfrastructure.setup(
         app_group_id: app_group.id,
         helm_cluster_template_id: helm_cluster_template.id,
-        )
+      )
 
       expect(helm_infrastructure).to be_persisted
     end
   end
 
   describe '.generate_cluster_index' do
-    it 'by default generates cluster index equal to cluster padding' do
+    it 'by default generates cluster index equal to padding when total count = 0' do
       expect(described_class.generate_cluster_index).to eq(described_class::CLUSTER_NAME_PADDING)
     end
 
-    it 'generates cluster index based on count' do
-      infrastructures = Array.new(3) { create(:helm_infrastructure) }
-      cluster_index = described_class::CLUSTER_NAME_PADDING + infrastructures.count
+    it 'generates cluster index based on total count and padding' do
+      helm_infrastructures = Array.new(3) { create(:helm_infrastructure) }
+      cluster_index = described_class::CLUSTER_NAME_PADDING + helm_infrastructures.count
       expect(described_class.generate_cluster_index).to eq(cluster_index)
+    end
+
+    it 'generates overlapping cluster index to infrastructure causing duplicate cluster names' do
+      total_count = 5
+      infrastructure_clusters = Array.new(total_count) do
+        create(:infrastructure)
+        Rufus::Mnemo.from_integer(Infrastructure.generate_cluster_index)
+      end.to_set
+      helm_infrastructure_clusters = Array.new(total_count) do
+        create(:helm_infrastructure)
+        Rufus::Mnemo.from_integer(HelmInfrastructure.generate_cluster_index)
+      end.to_set
+      expect(infrastructure_clusters).to eq(helm_infrastructure_clusters)
     end
   end
 end
