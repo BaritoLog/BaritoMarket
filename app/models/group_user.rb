8e8c4f0e33e2ab80515052e56a2938c5058b0e7e
Rayhan Gusty Alif <rayhan.alif@gopay.com>
2022-07-07 15:55:24 +0700

    Fix newly created user is not added into global-viewer group despite having the appropriate ENV

--- app/models/group_user.rb
+++ app/models/group_user.rb
@@ -3,6 +3,6 @@ class GroupUser < ActiveRecord::Base
   belongs_to :user
   belongs_to :role, class_name: 'AppGroupRole'
 
-  validates :group_id, :user_id, :role_id, presence: true
+  validates :group_id, :user_id, presence: true
   validates :group_id, uniqueness: { scope: :user_id }
 end
