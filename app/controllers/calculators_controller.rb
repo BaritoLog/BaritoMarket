9776941d5ebc86e4bc355a13aa296471e2ce13f1
Rayhan Gusty Alif <rayhangustyalif@gmail.com>
2024-01-09 11:54:57 +0700

    fix: change price calculator formula
    Co-authored-by: Abhinav Gupta <abhinavg1211@gmail.com>

--- app/controllers/calculators_controller.rb
+++ app/controllers/calculators_controller.rb
@@ -5,10 +5,10 @@ class CalculatorsController < ApplicationController
 	def calculate_price
     price_per_gb = Figaro.env.PRICE_PER_GB.to_f
     std_retention = Figaro.env.STANDARD_RETENTION.to_f
-    std_replication = Figaro.env.STANDARD_REPLICATION.to_f
+    # std_replication = Figaro.env.STANDARD_REPLICATION.to_f
     retention_multiplier = ((app_params[:retention].to_f - std_retention) / std_retention)
-    replication_multiplier = ((app_params[:replication].to_f - std_replication) / std_replication)
-    @price = sprintf("%0.9f", price_per_gb * app_params[:log_bytes].to_f * (1 + retention_multiplier + replication_multiplier))
+    # replication_multiplier = ((app_params[:replication].to_f - std_replication) / std_replication)
+    @price = sprintf("%0.6f", price_per_gb * app_params[:log_bytes].to_f * (1 + retention_multiplier))
     render json: @price
 	end
 
