f90ea204f2d30cc19b3faf3d2100a46dc05e2aef
clavinjune <24659468+clavinjune@users.noreply.github.com>
2024-08-12 20:00:53 +0700

    feat: introduce asphalt button
    Signed-off-by: clavinjune <24659468+clavinjune@users.noreply.github.com>

---
+++ ui/src/app/auth/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+import { Button } from '@asphalt-react/button';
+import variables from './page.module.scss';
+
+export default function Page() {
+  return <div>
+    <Button nude>asdasd</Button>
+    <h1 style={{ color: variables.primaryColor }}>helo</h1>
+  </div>
+  }
