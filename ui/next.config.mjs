d5571c84f8298a4e1aa1415edff18ebb52a78e98
clavinjune <24659468+clavinjune@users.noreply.github.com>
2024-08-12 19:18:30 +0700

    chore: enable autoreload when using docker for development
    Signed-off-by: clavinjune <24659468+clavinjune@users.noreply.github.com>

--- ui/next.config.mjs
+++ ui/next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: 'export',
+	webpack: (config, context) => {
+		config.watchOptions = {
+			poll: 1000,
+			aggregateTimeout: 300,
+		}
+		return config
+	},
 };
 
 export default nextConfig;
