d075b897555495660ec0e692761ac8026841e100
clavinjune <24659468+clavinjune@users.noreply.github.com>
2024-08-12 20:27:37 +0700

    chore: use auth page as root
    Signed-off-by: clavinjune <24659468+clavinjune@users.noreply.github.com>

--- ui/src/app/auth/page.tsx
+++ ui/src/app/auth/page.tsx
@@ -1,25 +1,5 @@
 'use client';
-import { AppbarHead, AppbarBody, AppbarTail, BaseAppbar } from '@asphalt-react/appbar';
-import { Button } from '@asphalt-react/button';
-import variables from './page.module.scss';
 
 export default function Page() {
-  return <main>
-    <BaseAppbar>
-      <AppbarHead>
-        <a href="/">Barito Log</a>
-      </AppbarHead>
-      <AppbarBody>
-        body
-      </AppbarBody>
-      <AppbarTail>
-        <div>Username</div>
-        <div>Sign Out</div>
-      </AppbarTail>
-    </BaseAppbar>
-  </main>
-  return <div>
-    <Button nude>asdasd</Button>
-    <h1 style={{ color: variables.primaryColor }}>helo</h1>
-  </div>
-  }
+  return <main>auth</main>
+}
