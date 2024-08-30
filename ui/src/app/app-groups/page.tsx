9277274750f9d46da117f02f928af8acf7dc4d5a
clavinjune <24659468+clavinjune@users.noreply.github.com>
2024-08-30 14:48:33 +0700

    feat: introduce readonly password component
    Signed-off-by: clavinjune <24659468+clavinjune@users.noreply.github.com>

--- ui/src/app/app-groups/page.tsx
+++ ui/src/app/app-groups/page.tsx
@@ -3,21 +3,41 @@
 import { useRouter, useSearchParams } from 'next/navigation'
 import { Button } from "@asphalt-react/button";
 import { Card } from "@asphalt-react/card";
-import { Edit, EyeOff } from "@asphalt-react/iconpack";
+import { Edit } from "@asphalt-react/iconpack";
 import { Stack } from "@asphalt-react/stack";
-import { Numeric, Password, Textfield } from "@asphalt-react/textfield";
+import { Password, Textfield } from "@asphalt-react/textfield";
 import { Text } from "@asphalt-react/typography";
 import styles from './page.module.scss';
+import { useEffect, useState } from 'react';
+import ReadOnlyPassword from '@/lib/readonly-password/ReadOnlyPassword';
 
 export default function Page() {
   const sp = useSearchParams()
   const router = useRouter()
+  const [appGroupData, setAppGroupData] = useState(null)
 
-  if (sp.get("id") === null) {
-    router.push('/')
+  useEffect(() => {
+    if (sp.get("id") === null) {
+      router.push('/')
+      return
+    }
+
+    const fetchAppGroupData = async () => {
+      try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_PATH ?? ''}/api/app-groups/${sp.get('id')}/`)
+        const data = await response.json()
+        setAppGroupData(data)
+      } catch (error) {
+        console.error('Error fetching app group data:', error)
+      }
+    }
+
+    fetchAppGroupData()
+  }, [sp, router])
+
+  if (!appGroupData) {
+    return <div>Loading...</div>
   }
-  console.log('url', `${process.env.NEXT_PUBLIC_BASE_PATH ?? ''}/api/app-groups/${sp.get('id')}/`)
-  fetch(`${process.env.NEXT_PUBLIC_BASE_PATH ?? ''}/api/app-groups/${sp.get('id')}/`).then(x => x.json()).then(x => console.log(x))
 
   return (
     <main>
@@ -33,22 +53,28 @@ export default function Page() {
 
             <div className={styles.textfield}>
               <Stack vertical>
-                  <Textfield stretch placeholder="First Name"
-                    addOnEnd={
-                      <Button icon nude compact system>
-                        <Edit />
-                      </Button>
-                    }
-                  />
-                <Textfield type="number" stretch placeholder="7"
+                <Textfield
+                  stretch
+                  placeholder="Application Group Name"
+                  value={appGroupData.name}
                   addOnEnd={
                     <Button icon nude compact system>
                       <Edit />
                     </Button>
                   }
                 />
-                <Password stretch defaultValue="example"/>
-
+                <Textfield
+                  type="number"
+                  stretch
+                  placeholder="Log Retention Days"
+                  value={appGroupData.log_retention_days}
+                  addOnEnd={
+                    <Button icon nude compact system>
+                      <Edit />
+                    </Button>
+                  }
+                />
+                <ReadOnlyPassword initialValue={appGroupData.secret}/>
               </Stack>
             </div>
           </Stack>
---
+++ ui/src/lib/readonly-password/ReadOnlyPassword.tsx
@@ -0,0 +1,39 @@
+'use client';
+import { Button } from '@asphalt-react/button';
+import { EyeOpen, EyeOff } from '@asphalt-react/iconpack';
+import { useState } from 'react';
+import { Input, InputWrapper, InputAddOn } from "@asphalt-react/textfield";
+
+interface ReadOnlyPasswordProps {
+    initialValue: string; // Prop to receive initial password value
+}
+
+const ReadOnlyPassword: React.FC<ReadOnlyPasswordProps> = ({ initialValue }) => {
+    const [isPasswordHidden, setIsPasswordHidden] = useState(true);
+
+    return (
+        <InputWrapper disabled>
+            <Input
+                stretch
+                type={isPasswordHidden ? "password" : "text"}
+                value={initialValue}
+                enclosed={false}
+                disabled
+                bezel={false}
+            />
+            <InputAddOn>
+                <Button 
+                    system 
+                    compact 
+                    nude 
+                    icon 
+                    onClick={() => setIsPasswordHidden(prev => !prev)}
+                >
+                    {isPasswordHidden ? <EyeOff /> : <EyeOpen />}
+                </Button>
+            </InputAddOn>
+        </InputWrapper>
+    );
+};
+
+export default ReadOnlyPassword;
